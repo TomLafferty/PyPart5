fa48bac5e5eca2dcdfc9d892609720a1dddbf368
TomLafferty <zipcode0>
2021-10-31 19:19:22 -0400

    exercise 3 done

--- palindrome.py
+++ palindrome.py
@@ -1,4 +1,10 @@
 def is_palindrome(value: str) -> bool:
+    """
+        This function determines if a word or phrase is a palindrome
+
+        :param value: A string
+        :return: A boolean
+        """
     lowered = value.lower()
     base = lowered.replace(" ","")
     if base[::-1] == base:
@@ -7,10 +13,5 @@ def is_palindrome(value: str) -> bool:
         return False
 
 
-    """
-    This function determines if a word or phrase is a palindrome
 
-    :param value: A string
-    :return: A boolean
-    """
       # remove pass statement and implement me
